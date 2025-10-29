40cbc15fd62ab1e829e055dd65e9185f8b427ff1
matei1998 <153720444+matei19989@users.noreply.github.com>
2025-10-29 10:24:29 +0200

    Delete FraudDetection.Application/Requests/Commands/UpdateFraudRulePriorityCommand .cs

--- FraudDetection.Application/Requests/Commands/UpdateFraudRulePriorityCommand .cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-
-namespace FraudDetection.Application.Requests.Commands;
-
-public record UpdateFraudRulePriorityCommand : IRequest<bool>
-{
-    public required Guid RuleId { get; init; }
-    public required int NewPriority { get; init; }
-}
